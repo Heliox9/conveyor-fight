7fdae9d25b4878fde4c9dfae473a324178aba333
Jonas <j.hasenauer98@gmail.com>
2021-05-26 15:34:29 +0200

    set orientation to landscape

--- conveyor_fight/app/src/main/java/de/conveyorfight/MainActivity.kt
+++ conveyor_fight/app/src/main/java/de/conveyorfight/MainActivity.kt
@@ -1,11 +1,13 @@
 package de.conveyorfight
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.ActivityInfo
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
     }
 }
